4a3d372e07e63898a3e9d3f9062e357d0bd3088e
dsk1513 <dc697@njit.edu>
2023-11-06 18:49:48 -0500

    updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'docs')));
 
 // Define the route for the homepage
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'docs', 'index.html')); 
+  res.sendFile(path.join(__dirname, 'docs', 'Home.html')); 
 });
 
 // Start the server
